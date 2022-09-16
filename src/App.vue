626374a811ad7bc0417f1afacf0811c32c381815
Michael Strating <michaelhstrating@gmail.com>
2022-09-15 23:05:49 -0400

    now can logout and page responds to login status

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,24 @@
 <script setup>
-import { RouterLink, RouterView } from 'vue-router'
+import { RouterLink, RouterView, useRouter } from 'vue-router'
+import { getAuth } from 'firebase/auth'
+import { ref } from 'vue';
+
+const router = useRouter()
+const isLoggedIn = ref(false)
+const auth = getAuth()
+
+auth.onAuthStateChanged(user => {
+  if (user) {
+    isLoggedIn.value = true
+  } else {
+    isLoggedIn.value = false
+  }
+})
+
+function signOut() {
+  auth.signOut()
+  router.push('/')
+}
 </script>
 
 <template>
@@ -8,8 +27,11 @@ import { RouterLink, RouterView } from 'vue-router'
       <nav>
         <RouterLink to="/">Home</RouterLink>
         <RouterLink to="/about">About</RouterLink>
-        <RouterLink to="/sign-in">Login</RouterLink>
-        <RouterLink to="/register">Register</RouterLink>
+        <span v-if="isLoggedIn"><button @click="signOut">Logout</button></span>
+        <span v-else>
+          <RouterLink to="/sign-in">Login</RouterLink>
+          <RouterLink to="/register">Register</RouterLink>
+        </span>
       </nav>
     </div>
   </header>
