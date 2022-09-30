1b675560f700aa60f2dd45267704551de894dd28
Michael Strating <michaelhstrating@gmail.com>
2022-09-30 14:10:21 -0400

    super basic beginnings of the design language I am going for

--- src/App.vue
+++ src/App.vue
@@ -1,51 +1,59 @@
 <script setup>
-import { RouterLink, RouterView, useRouter } from 'vue-router'
-import { getAuth } from 'firebase/auth'
-import { ref } from 'vue';
-
-const router = useRouter()
-const isLoggedIn = ref(false)
-const auth = getAuth()
-
-auth.onAuthStateChanged(user => {
-  if (user) {
-    isLoggedIn.value = true
-  } else {
-    isLoggedIn.value = false
-  }
-})
-
-function signOut() {
-  auth.signOut()
-  router.push('/')
-}
 </script>
 
 <template>
   <header class="header">
-    <div class="wrapper">
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-        <span v-if="isLoggedIn"><button @click="signOut">Logout</button></span>
-        <span v-else>
-          <RouterLink to="/sign-in">Login</RouterLink>
-          <RouterLink to="/register">Register</RouterLink>
-        </span>
-        <RouterLink to="/create">Create</RouterLink>
-      </nav>
+    <div>
+      <RouterLink to="/" class="header-title">Blasted Book Blog</RouterLink>
+      <!-- <span class="header-author"> by Sigath</span> -->
     </div>
+    <nav>
+      <RouterLink to="/">Home</RouterLink>
+      <RouterLink to="/about">About</RouterLink>
+    </nav>
   </header>
-  <!-- <RouterView /> -->
-  <footer class="footer"></footer>
+  <RouterView />
+  <footer class="footer">
+    <nav>
+      <RouterLink to="/">Home</RouterLink>
+      <RouterLink to="/about">About</RouterLink>
+    </nav>
+    <span>Copyright &copy; 2022 Blasted Book Blog</span>
+  </footer>
 </template>
 
 <style scoped>
   .header {
+    display: flex;
+    justify-content: space-between;
+    align-items: flex-end;
+
     height: 100px;
+    padding-bottom: 10px;
+
+    border-style: solid;
+    border-color: var(--color-background-mute);
+    border-width: 0px 0px 5px 0px;
+  }
+  .header-title {
+    font-weight: 400;
+    font-size: 20px;
+    color: var(--color-background-soft);
+  }
+
+  .header-author {
+    color: var(--color-text);
   }
 
   .footer {
-    height: 100px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  }
+
+  nav {
+    display: flex;
+    justify-content: space-evenly;
+    gap: 10px;
   }
 </style>
---
+++ src/views/AboutView.vue
@@ -0,0 +1,8 @@
+<script setup>
+</script>
+<template>
+  <main class="main">
+  </main>
+</template>
+<style scoped>
+</style>
