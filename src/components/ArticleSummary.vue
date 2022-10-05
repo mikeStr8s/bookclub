ce98ed87bdf05bec43e7916ef5309c1b77f435a9
Michael Strating <michaelhstrating@gmail.com>
2022-10-05 18:56:46 -0400

    got a bit more styling done. Finished welcome and now working on a most recent slider

--- src/components/ArticleSummary.vue
+++ src/components/ArticleSummary.vue
@@ -1,27 +1,45 @@
 <script setup>
+  import { ref } from 'vue';
 
-const props = defineProps({
-  content: String,
-  date: Number,
-  title: String,
-  ups: Number,
-  parent: String,
-  comments: Number
-})
+  const props = defineProps({
+    content: String,
+    date: Number,
+    title: String,
+    ups: Number,
+    parent: String,
+    comments: Number
+  })
+
+  const content = ref('')
+  content.value = props.content.substring(0, 249) + '...'
+
+  const date = ref('')
+  const date_obj = new Date(props.date)
+  date.value = date_obj.toLocaleDateString( 'en-us',{
+    year: 'numeric',
+    month: 'long',
+    day: 'numeric'
+  })
+
 </script>
 
 <template>
-<div class="recent">
-  <h3>{{ props.title }}</h3>
-  <span class="date">{{ props.date }}</span>
-  <p class="post-body">{{ props.content }}</p>
-  <div class="article-summary">
-    <a href="">{{ props.comments }} Comment/s</a>
-    <!-- <input type="checkbox" name="ups-input" id="ups-input"> -->
-    <span>{{ props.ups }}</span>
+  <div class="recent">
+    <h3>{{ props.title }}</h3>
+    <span class="date">{{ date }}</span>
+    <p class="post-body">{{ content }}</p>
+    <div class="article-summary">
+      <a href="">{{ props.comments }} Comment/s</a>
+      <!-- <input type="checkbox" name="ups-input" id="ups-input"> -->
+      <span>{{ props.ups }} ↑</span>
+    </div>
   </div>
-</div>
 </template>
 
 <style scoped>
+  .article-summary {
+    display: flex;
+    flex-direction: row;
+    gap: 10px;
+  }
 </style>
--- src/components/MostRecent.vue
+++ src/components/MostRecent.vue
@@ -1,32 +1,82 @@
 <script setup>
-import ArticleSummary from './ArticleSummary.vue';
-import { ref } from 'vue';
+  import ArticleSummary from './ArticleSummary.vue';
+  import { ref } from 'vue';
 
-const most_recent = ref({
-  content: `You are Rustopherson, a wizard living in the kingdom of Larion. You have a staff and a spellbook.
-  You finish your long journey and finally arrive at the ruin you've been looking for.
-  You have come here searching for a mystical spellbook of great power called the book of essence.
-  You look around and see no one in the area.
-  You know the danger of the open plains and the undead marauders that dwell there, so you don't stay in one place too long.`,
-  date: 1663876271133,
-  title: 'The Tale as Old as Time Or: How I Really Forgot What I',
-  ups: 123,
-  // tags: ['fantasy', 'silly-billy', 'absolute-menace'],
-  parent: '/',
-  comments: 3
-})
+  const most_recent = ref({
+    content: `You are Rustopherson, a wizard living in the kingdom of Larion. You have a staff and a spellbook.
+    You finish your long journey and finally arrive at the ruin you've been looking for.
+    You have come here searching for a mystical spellbook of great power called the book of essence.
+    You look around and see no one in the area.
+    You know the danger of the open plains and the undead marauders that dwell there, so you don't stay in one place too long.`,
+    date: 1663876271133,
+    title: 'The Tale as Old as Time Or: How I Really Forgot What I',
+    ups: 123,
+    // tags: ['fantasy', 'silly-billy', 'absolute-menace'],
+    parent: '/',
+    comments: 3
+  })
 
+  const t1 = ref("Title1")
+  const t2 = ref("Title2")
+  const t3 = ref("Title3")
+
+  let slide_idx = 0;  
+
+  function advanceSlide(idx) {
+    slide_idx = slide_idx + idx
+    showSlide(slide_idx)
+  }
+
+  function showSlide(idx) {
+    if (idx > 2) { slide_idx = 0 }
+    if (idx < 0) { slide_idx = 2 }
+    const article_elements = document.getElementsByClassName('recent')
+    for (let i  = 0; i < article_elements.length; i++) { article_elements[i].style.display = 'none'}
+    article_elements[slide_idx].style.display = 'block'
+  }
 </script>
 
 <template>
-<div class="most-recent-container">
-  <a href="">Most Recent</a>
-  <div class="most-recent-list">
-    <ArticleSummary :content="most_recent.content" :date="most_recent.date" :title="most_recent.title"
-    :ups="most_recent.ups" :parent="most_recent.parent" :comments="most_recent.comments"></ArticleSummary>
+  <div class="most-recent-container">
+    <a href="">Most Recent</a>
+    <div class="slider">
+      <button class="blog-btn" @click="advanceSlide(-1)">&lt;</button>
+      <div class="most-recent-list">
+        <ArticleSummary style="display:block;" :content="most_recent.content" :date="most_recent.date" :title="t1"
+        :ups="most_recent.ups" :parent="most_recent.parent" :comments="most_recent.comments"></ArticleSummary>
+        <ArticleSummary :content="most_recent.content" :date="most_recent.date" :title="t2"
+        :ups="most_recent.ups" :parent="most_recent.parent" :comments="most_recent.comments"></ArticleSummary>
+        <ArticleSummary :content="most_recent.content" :date="most_recent.date" :title="t3"
+        :ups="most_recent.ups" :parent="most_recent.parent" :comments="most_recent.comments"></ArticleSummary>
+      </div>
+      <button class="blog-btn" @click="advanceSlide(1)">&gt;</button>
+    </div>
   </div>
-</div>
 </template>
 
 <style scoped>
+  .most-recent-container {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+  }
+
+  .most-recent-list {
+    flex-direction: row;
+  }
+
+  .slider {
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+  }
+  
+  .blog-btn {
+    padding-left: 5px;
+    padding-right: 5px;
+  }
+
+  .recent {
+    display: none;
+  }
 </style>
